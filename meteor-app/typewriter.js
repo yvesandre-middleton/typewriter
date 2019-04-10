f6c7e6738781de854955a9a3a241043b6c1ea274
Gwyn Morfey <mail@gwynmorfey.com>
2019-04-10 11:00:34 +0100

    Updated

--- meteor-app/typewriter.js
+++ meteor-app/typewriter.js
@@ -1,6 +1,7 @@
+// import { position, offset } from 'caret-pos';
 
 Texts = new Mongo.Collection("texts");
-
+console.log("111234");
 function updateText(newText) {
   var theText = Texts.findOne({}, {sort: {createdAt: -1}});
 
@@ -15,7 +16,6 @@ function updateText(newText) {
 }
 
 if (Meteor.isClient) {
-
   Template.body.helpers({
     text: function () {
       return Texts.findOne({}, {sort: {createdAt: -1}});
@@ -24,6 +24,13 @@ if (Meteor.isClient) {
 
   Template.body.events({
     "click #sync-button": function (event) {
+
+
+      // const input = document.querySelector('#text-editor');
+      // // const pos = position(input); // { left: 15, top: 30, height: 20, pos: 15 }
+      // const off = offset(input); // { left: 15, top: 30, height: 20 }
+      // console.log(offset.left, offset.top, offset.height);
+
       updateText(document.getElementById("text-editor").innerHTML);
     }
   });
