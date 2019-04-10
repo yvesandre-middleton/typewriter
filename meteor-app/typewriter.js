d3911273e79cf669b66f69fbd4d96faea48c1046
Gwyn Morfey <mail@gwynmorfey.com>
2019-04-10 11:48:16 +0100

    WIP

--- meteor-app/typewriter.js
+++ meteor-app/typewriter.js
@@ -1,15 +1,17 @@
-// import { position, offset } from 'caret-pos';
+import { position, offset } from 'caret-pos';
 
 Texts = new Mongo.Collection("texts");
-console.log("111234");
-function updateText(newText) {
+function updateText(newText,newLeft,newTop,newHeight) {
   var theText = Texts.findOne({}, {sort: {createdAt: -1}});
 
   if (theText) {
-    Texts.update(theText._id, {$set: {text: newText}});
+    Texts.update(theText._id, {$set: {text: newText, left: newLeft, top:newTop, height: newHeight}});
   } else {
     theText = {};
     theText.text = newText;
+    theText.left = 0;
+    theText.top = 0;
+    theText.height = 0;
     Texts.insert(theText);
   }
 
@@ -26,12 +28,10 @@ if (Meteor.isClient) {
     "click #sync-button": function (event) {
 
 
-      // const input = document.querySelector('#text-editor');
-      // // const pos = position(input); // { left: 15, top: 30, height: 20, pos: 15 }
-      // const off = offset(input); // { left: 15, top: 30, height: 20 }
-      // console.log(offset.left, offset.top, offset.height);
-
-      updateText(document.getElementById("text-editor").innerHTML);
+      const input = document.querySelector('#text-editor');
+      const pos = position(input); // { left: 15, top: 30, height: 20, pos: 15 }
+      const cursorDiv = document.querySelector("#cursor");
+      updateText(document.getElementById("text-editor").innerHTML,pos.left+7,pos.top+7,pos.height);
     }
   });
 
